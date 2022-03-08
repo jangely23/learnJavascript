e530ba75c91f99529ec172c3abca40c002ab8832
jessica leonel <jangely23@hotmail.com>
2022-03-07 21:12:44 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//It is Javascript for document
+
+const  line_one= "No es sobre las ideas. Sino sobre hacer que estas se vuelvan realidad.";
+const  line_two= "La manera de empezar es dejar de hablar y empezar a hacer.";
+
+document.getElementById()
+
+
